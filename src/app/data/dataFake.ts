1975b3cdf2af4c1c66f11ae974b0563ee60284a5
gabrielpgf <gabrielpgf2011@gmail.com>
2023-05-13 13:08:21 -0300

    conteúdo criado

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "photo": "https://th.bing.com/th/id/OIP.nPG8T0Rvfyd8YDYVxDwYlwHaEK?w=313&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+    "title": "Marvel Studios anuncia novo filme do Homem de Ferro.",
+    "description": "NOVO HOMEM DE FERRO ANUNCIADO"
+  },
+  {
+    "id": "2",
+    "photo": "https://kenh14cdn.com/203336854389633024/2021/2/18/photo-1-1613635174418119354960.jpg",
+    "title": "Nova série anuncuada no Disney +",
+    "description": "tchau"
+  }
+]
---
+++ src/app/pages/content/content.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { dataFake } from '../../data/dataFake';
+import { Attribute } from '@angular/compiler';
+
+@Component({
+  selector: 'app-content',
+  templateUrl: './content.component.html',
+  styleUrls: ['./content.component.css']
+})
+export class ContentComponent implements OnInit {
+  photoCover:string = '';
+  contentTitle:string = 'NOTICIA DE EXEMPLO';
+  contentDescription:string = 'bla bla bla bla';
+  private id:string | null = '0';
+
+  constructor(private route:ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe( value =>
+      this.id = value.get('id')
+    )
+
+    this.setValuesToComponent(this.id)
+  }
+
+  setValuesToComponent(id:string | null){
+    const result = dataFake.filter(article => article.id == id)[0]
+
+    this.photoCover = result.photo;
+    this.contentTitle = result.title;
+    this.contentDescription = result.description;
+  }
+}
